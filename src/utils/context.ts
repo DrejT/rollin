0bd431b84526cd6604cb668e4ea7bab18e6cc16a
DrejT <vivekpatel4049@gmail.com>
2024-01-22 11:13:44 +0530

    updated ui components

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -3,6 +3,8 @@ import { Dispatch, SetStateAction, createContext } from "react";
 export interface globalContextProps {
   fetchNotes: boolean;
   setFetchNotes: Dispatch<SetStateAction<boolean>>;
+  fetchCategories: boolean;
+  setfetchCategories: Dispatch<SetStateAction<boolean>>;
 }
 
 export const GlobalContext = createContext<globalContextProps | undefined>(
